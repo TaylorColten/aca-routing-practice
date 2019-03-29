789abc807f96779f3dec39fe2bce9dd609a04946
unknown <taylorwcolten@gmail.com>
2019-03-28 20:09:21 -0500

    router

--- src/index.js
+++ src/index.js
@@ -3,10 +3,13 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import Header from './components/Header'
+import { BrowserRouter } from "react-router-dom";
+import Router from "./Router";
 
 const Complete = () => (
   <Fragment>
     <Header />
+    <Router />
     <App />
   </Fragment>
 )
