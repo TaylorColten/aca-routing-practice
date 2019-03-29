7393c13c5ee66173da250723d4b803424d58e2f8
unknown <taylorwcolten@gmail.com>
2019-03-28 20:20:09 -0500

    ul and linkss

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,9 +1,14 @@
 import React from 'react'
 import './Header.css'
+import {Link} from "react-router-dom";
 
 const Header = () => (
   <div className="header">
-    <p>this is the header</p>
+    <ul>
+      <Link to="/">Landing</Link>
+      <Link to="/home">Home</Link>
+      <Link to="/about">About</Link>
+    </ul>
   </div>
 )
 
